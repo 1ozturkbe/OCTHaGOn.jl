3b5b72c5de92f69fa561fd9dbda1a2a208ecc35b
1ozturkbe <1ozturkbe@gmail.com>
2020-04-13 15:32:30 -0400

    Change the way blockPrint works (fix color glitch).

--- OptimalConstraintTree/tools.py
+++ OptimalConstraintTree/tools.py
@@ -1,14 +1,17 @@
 import progressbar
 from time import sleep
 import sys, os
+import io
 
 from gpfit.fit_constraintset import FitCS
+
+text_trap = io.StringIO()
 
 # Disable or enable printing for problematic functions
 def blockPrint():
     """ Disables printing from this onward.
     Printing MUST BE REACTIVATED after. """
-    sys.stdout = open(os.devnull, 'w')
+    sys.stdout = text_trap
 
 def enablePrint():
     """ Enables print outputs.
@@ -38,28 +41,28 @@ def find_signomials(model):
     """
     pass
 
-def constraint_from_gpfit(fitcs, ivar, dvars, basis=None):
+def constraint_from_gpfit(fitcs, dvar, ivars, basis=None):
     """
     Returns a posynomial constraint from a data fit
     :param fitcs: FitCS object
-    :param ivar: independent variable
-    :param dvars: dependent variables
+    :param dvar: dependent variable
+    :param ivars: independent variables
     :return: constraint
     """
     basis_keys = list(basis.keys())
-    dvar_keys = [dvar.key for dvar in dvars]
+    ivar_keys = [ivar.key for ivar in ivars]
     try:
-        nondim_ivar = ivar/basis[ivar.key]
+        nondim_dvar = dvar/basis[dvar.key]
     except KeyError:
         raise KeyError("Independent variable %s "
                        "is not in the basis to be "
-                       "normalized" % ivar.key)
-    basis.pop(ivar.key)
+                       "normalized" % dvar.key)
+    basis.pop(dvar.key)
     try:
-        nondim_dvars = [dvars[i]/basis[basis_keys[i]] for i in range(len(basis.keys()))]
+        nondim_ivars = [ivars[i]/basis[basis_keys[i]] for i in range(len(basis.keys()))]
     except KeyError:
         raise KeyError("There is a dependent variable "
                        "missing in the basis, or there is an "
                        "incorrect type. ")
-    new_fitcs = FitCS(fitcs.fitdata, nondim_ivar, nondim_dvars)
+    new_fitcs = FitCS(fitcs.fitdata, nondim_dvar, nondim_ivars)
     return new_fitcs
