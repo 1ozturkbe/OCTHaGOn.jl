22e2331bf80fb9005551204608846fa7f36d0890
1ozturkbe <1ozturkbe@gmail.com>
2020-06-16 11:20:56 -0400

    Proper bounding.

--- julia/test/examples.jl
+++ julia/test/examples.jl
@@ -5,7 +5,7 @@ const MOI = MathOptInterface
 using MathProgBase
 using ConicBenchmarkUtilities
 
-include("../src/structs.jl")
+include("../src/structs.jl");
 
 function alphac_to_fn(alpha, c; lse=false)
     nterms, xdim = size(alpha)
@@ -80,8 +80,8 @@ function CBF_to_MOF(filename, solver=Mosek.Optimizer())
 end
 
 # function MOF_to_fnmodel(mof_model)
-    inner_variables = MOI.get(mof_model, MOI.ListOfVariableIndices())
-    constraints = MOI.get(mof_model, MOI.ListOfConstraints())
+#     inner_variables = MOI.get(mof_model, MOI.ListOfVariableIndices())
+#     constraints = MOI.get(mof_model, MOI.ListOfConstraints())
 #     constraint_indices = MOI.get(mof_model, MOI.ListOfConstraintIndices)
 #     variable_attributes_set = MOI.get(mof_model, MOI.ListOfVariableAttributesSet())
 #     #TODO: CONTINUE HERE
@@ -139,17 +139,28 @@ function CBF_to_fnmodel(filename)
     eqs = [];
     eq_idxs = Array[];
     for (cone, idxs) in constr_cones
+    # Idxs describe which rows of A the cone applies to.
     # All cones: [:Free, :Zero, :NonNeg, :NonPos, :SOC, :SOCRotated, :SDP, :ExpPrimal, :ExpDual]
         if cone == :NonNeg
-            constr_fn = x -> b[idxs] - A[:,idxs]*x;
-            push!(ineqs, constr_fn);
-            push!(ineq_idxs, idxs);
+            if length(A[idxs,:].nzval) > 1.
+                constr_fn = x -> b[idxs] - A[idxs, :]*x;
+                push!(ineqs, constr_fn);
+                push!(ineq_idxs, idxs);
+            else
+                l[findall(!iszero, A[idxs,:])[1][2]] = maximum([l[findall(!iszero, A[idxs,:])[1][2]],
+                                                                b[idxs][1]/A[idxs, :].nzval[1]]);
+            end
         elseif cone == :NonPos
-            constr_fn = x -> -1 .* b[idxs] + A[:,idxs]*x;
-            push!(ineqs, constr_fn);
-            push!(ineq_idxs, idxs);
+            if length(idxs) > 1.
+                constr_fn = x -> -1 .* b[idxs] + A[idxs, :]*x;
+                push!(ineqs, constr_fn);
+                push!(ineq_idxs, idxs);
+            else
+                u[findall(!iszero, A[idxs,:])[1][2]] = minimum([u[findall(!iszero, A[idxs,:])[1][2]],
+                                                   b[idxs][1]/A[idxs, :].nzval[1]]);
+            end
         elseif cone == :Zero
-            constr_fn = x -> b[idxs] - A[:,idxs]*x;
+            constr_fn = x -> b[idxs] - A[idxs, :]*x;
             push!(eqs, constr_fn);
             push!(eq_idxs, idxs);
         elseif cone == :SOC
@@ -187,9 +198,9 @@ mof_model = CBF_to_MOF(filename);
 MOI.optimize!(mof_model);
 status = MOI.get(mof_model, MOI.TerminationStatus());
 mof_obj = MOI.get(mof_model, MOI.ObjectiveValue());
-# @test mof_obj ≈ 3.968224074
+@test mof_obj ≈ 3.968224074
 
 # Getting fn_model using MPB
-filename = "../../data/cblib.zib.de/flay03m.cbf.gz"
-fn_model = CBF_to_fnmodel(filename)
+filename = "../../data/cblib.zib.de/flay03m.cbf.gz";
+fn_model = CBF_to_fnmodel(filename);
 
