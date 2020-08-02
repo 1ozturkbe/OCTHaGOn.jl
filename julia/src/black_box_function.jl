dabf2e0e1e737192664269755c73bebade5a3c9f
1ozturkbe <1ozturkbe@gmail.com>
2020-08-02 15:22:50 -0400

    BBF is now based on vks, not idxs. DataFrames galore.

--- julia/src/black_box_function.jl
+++ julia/src/black_box_function.jl
@@ -21,35 +21,35 @@ Contains all required info to be able to generate a global optimization problem.
 """
     name::Union{String, Int} = ""                      # Function name
     fn::Function                                       # The function
-    idxs::Union{Array, Nothing} = nothing              # Variable indices
-    lbs::Array = []                                    # Variable lower bounds
-    ubs::Array = []                                    # Variable upper bounds
-    samples::Union{Nothing, DataFrame} = nothing       # Function samples
-    values::Array = []                                 # Function values
+    vks::Array                                         # Varkeys
+    lbs::Dict = Dict(vks .=> -Inf)                     # Variable lower bounds
+    ubs::Dict = Dict(vks .=> Inf)                      # Variable upper bounds
+    X::DataFrame = DataFrame([Float64 for i in vks], vks)  # Function samples
+    Y::Array = []                                      # Function values
     feas_ratio::Float64 = 0.                           # Feasible sample proportion
-    feas_min::Float64 = 0.1                            # Minimum feas_ratio
     equality::Bool = false                             # Equality check
     gp::Union{GPE, Nothing} = nothing                  # Gaussian Process
     learners::Array{IAI.GridSearch} = []               # Learners...
     constraints::Array{Array} = Array[]                # and their corresponding constraints,
     accuracies::Array{Float64} = []                    # and their scores.
+    threshold_accuracy::Float64 = 0.95                 # Minimum tree accuracy
+    threshold_feasibility::Float64 = 0.25              # Minimum feas_ratio
     n_samples::Int = 100                               # For next set of samples.
     tags::Array{String} = []                           # Other tags
 end
 
-function (bbf::BlackBoxFn)(x)
+function (bbf::BlackBoxFn)(x::Union{DataFrameRow, Dict})
     return bbf.fn(x)
 end
 
 function eval!(bbf::BlackBoxFn, X::Union{AbstractArray, DataFrame})
-    df = DataFrame(X);
     if isnothing(bbf.X)
-        bbf.X = df;
-        bbf.Y = [bbf(Array(df[i,:])) for i=1:size(df,1)];
+        bbf.X = X;
+        bbf.Y = [bbf(X[i,:]) for i=1:size(X,1)];
         bbf.feas_ratio = sum(bbf.Y .>= 0)/length(bbf.Y);
     else
-        values = [bbf(df[i,:]) for i=1:size(df,1)];
-        append!(bbf.X, df, cols=:setequal);
+        values = [bbf(X[i,:]) for i=1:size(X,1)];
+        append!(bbf.X, X, cols=:setequal);
         append!(bbf.Y, values);
         bbf.feas_ratio = sum(bbf.Y .>= 0)/length(bbf.Y); #TODO: optimize.
     end
@@ -78,17 +78,18 @@ end
 function sample_and_eval!(bbf::BlackBoxFn; ratio=10.)
     """ Samples and evaluates BlackBoxFn, with n_samples new samples.
     ratio*n_samples is how many random LHC samples are generated for prediction from GP. """
-    n_dims = length(bbf.idxs);
+    vks = bbf.vks;
+    n_dims = length(vks);
     if isnothing(bbf.gp) # If we don't have any GPs yet, uniform sample.
        plan, _ = LHCoptim(bbf.n_samples, n_dims, 3);
-       if any(isinf.(hcat(bbf.lbs, bbf.ubs)))
+       if any(isinf.(values(bbf.lbs))) || any(isinf.(values(bbf.ubs)))
            throw(ArgumentError("Model is not properly bounded."))
        end
-       df = DataFrame(scaleLHC(plan,[(bbf.lbs[i], bbf.ubs[i]) for i=1:n_dims]));
+       df = DataFrame(scaleLHC(plan,[(bbf.lbs[i], bbf.ubs[i]) for i in vks]), vks);
        eval!(bbf, df);
     else
         plan = randomLHC(Int(round(bbf.n_samples*ratio)), n_dims);
-        random_samples = scaleLHC(plan,[(bbf.lbs[i], bbf.ubs[i]) for i=1:n_dims]);
+        random_samples = scaleLHC(plan,[(bbf.lbs[i], bbf.ubs[i]) for i in vks]);
         μ, σ = predict_y(bbf.gp, random_samples');
         cdf_0 = [Distributions.cdf(Distributions.Normal(μ[i], σ[i]),0) for i=1:size(random_samples,1)];
          #TODO: add criterion for information as well (something like sortperm(σ))
@@ -97,7 +98,8 @@ function sample_and_eval!(bbf::BlackBoxFn; ratio=10.)
         p = bbf.feas_ratio
         balance_point = -1/0.5^2*(p-0.5)^3 + 0.5;
         indices = sortperm(abs.(cdf_0 .- balance_point));
-        eval!(bbf, random_samples[indices[1:bbf.n_samples],:])
+        samples = DataFrame(random_samples[indices[1:bbf.n_samples],:], vks);
+        eval!(bbf, samples);
     end
     return
 end
--- julia/test/test_bbf.jl
+++ julia/test/test_bbf.jl
@@ -8,26 +8,42 @@ Tests BlackBoxFn and its functions, including GaussianProcesses
 using Test
 using GaussianProcesses
 using Plots
+using Random
 
 include("../src/OptimalConstraintTree.jl")
 global OCT = OptimalConstraintTree;
 global PROJECT_ROOT = @__DIR__
 
-bbf = OCT.BlackBoxFn(fn = x -> x[1]^2 * sin(x[1]) + 2,
-                    idxs = [1], lbs = [-5.], ubs = [5.],
+bbf = OCT.BlackBoxFn(fn = x -> x[:x1] + x[:x3]^3,
+                    vks = [:x1, :x3], lbs = Dict(:x1 => -5, :x3 => -5),
+                                      ubs = Dict(:x1 => 5, :x3 => 5),
                     n_samples = 50);
-x = range(-5,stop=5, step=0.1);
-plot(x, bbf.fn.(x))
+
+# Check evaluation of samples from Dict and DataFrame
+samples = DataFrame(rand(4,4), [Symbol("x",i) for i=1:4]);
+val1 = bbf(samples[1,:]);
+sample = Dict(:x1 => samples[1,1], :x3 => samples[1,3])
+val2 = bbf(sample);
+@test val1 == val2 == samples[1,1] + samples[1,3]^3;
+
+# Check evaluation
+OCT.sample_and_eval!(bbf)
+
+# bbf = OCT.BlackBoxFn(fn = x -> x[1]^2 * sin(x[1]) + 2,
+#                     idxs = [1], lbs = [-5.], ubs = [5.],
+#                     n_samples = 50);
+# x = range(-5,stop=5, step=0.1);
+# plot(x, bbf.fn.(x))
 
 # Sampling and plotting raw data.
-OCT.sample_and_eval!(bbf);
-OCT.optimize_gp!(bbf)
-plot!(bbf.gp)
-
-# # Sample and plot again
-OCT.sample_and_eval!(bbf);
-OCT.optimize_gp!(bbf)
-plot!(bbf.gp)
-
-# Finally learning constraint
-OCT.learn_constraint!(bbf);
+# OCT.sample_and_eval!(bbf);
+# OCT.optimize_gp!(bbf)
+# plot!(bbf.gp)
+#
+# # # Sample and plot again
+# OCT.sample_and_eval!(bbf);
+# OCT.optimize_gp!(bbf)
+# plot!(bbf.gp)
+#
+# # Finally learning constraint
+# OCT.learn_constraint!(bbf);
