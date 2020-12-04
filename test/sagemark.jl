9dfe427a9cfadb3a849825c86f48300f314bfa52
Berk Ozturk <1ozturkbe@gmail.com>
2020-12-04 14:16:55 -0500

    Cleaning up dependencies.  (#14)
    * Removing PyCall from dependencies.
    
    * Cleaning up Project.toml.
    
    * sagemark and PyCall as well.
    
    * Removing silly tests.

--- test/sagemark.jl
+++ test/sagemark.jl
@@ -1,10 +1,13 @@
 #=
-tools:
+test_tools:
 - Julia version: 
 - Author: Berk
 - Date: 2020-06-17
-Adds tools to mainly import optimization problems from other sources.
+Tests tools.
 =#
+
+using PyCall
+using Ipopt
 
 function alphac_to_expr(model, alpha, c; lse=false)
     """ Turns exponential to JuMP.NonlinearExpression. """
@@ -50,7 +53,7 @@ function sagemark_to_GlobalModel(idx; lse=false)
     Arguments:
         idx:: number of benchmark
     Returns:
-        md:: a function_model
+        gm:: a function_model
     """
     sagemarks = pyimport("sagebenchmarks.literature.solved");
     signomials, solver, run_fn = sagemarks.get_example(idx);
@@ -84,3 +87,69 @@ function sagemark_to_GlobalModel(idx; lse=false)
     return gm
 end
 
+function test_sagemark_to_GlobalModel()
+    """ Makes sure all sage benchmarks import properly.
+        For now, just doing first 25, since polynomial
+        examples are not in R+. """
+    @info("Testing sagemark imports.")
+    idxs = 1:25;
+    # max_min = [26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
+    for idx in idxs
+        ex = sagemark_to_GlobalModel(idx);
+    end
+    idx = 1;
+    gm = sagemark_to_GlobalModel(idx; lse=false);
+    gm_lse = sagemark_to_GlobalModel(idx; lse=true);
+    classify_constraints.([gm, gm_lse])
+    return true
+end
+
+# Importing sagebenchmark to ModelData and checking it
+@test test_sagemark_to_GlobalModel()
+gm = sagemark_to_GlobalModel(1, lse=false);
+gm_lse = sagemark_to_GlobalModel(1, lse=true);
+# set_optimizer(gm, Ipopt.Optimizer)
+# nonlinearize!(gm)
+# optimize!(gm)
+# vals = getvalue.(gm.vars)
+
+bounds = Dict(all_variables(gm) .=> [[0.1, 1], [5., 10.], [8., 15.], [0.01, 1.], [-Inf, Inf]])
+@test all(get_bounds(gm)[var] ≈ bounds[var] for var in all_variables(gm))
+inp = Dict(all_variables(gm) .=> [1,1.9,3,3.9, 10.]);
+log_inp = Dict(vk => log(val) for (vk, val) in inp);
+
+@test gm.vars == all_variables(gm);
+@test gm_lse.bbfs[1](log_inp)[1] ≈ gm.bbfs[1](inp)[1] ≈ [inp[gm.vars[5]] - inp[gm.vars[3]] ^ 0.8 * inp[gm.vars[4]] ^ 1.2][1]
+
+# Checking OCTException for sampling unbounded model
+@test_throws OCTException sample_and_eval!(gm.bbfs[1], n_samples=200)
+
+# Actually trying to optimize...
+gm = sagemark_to_GlobalModel(3; lse=false);
+set_optimizer(gm, Gurobi.Optimizer)
+bound!(gm, Dict(gm.vars[end] => [-300, 0]))
+sample_and_eval!(gm, n_samples = 500)
+sample_and_eval!(gm, n_samples = 500)
+
+learn_constraint!(gm)
+println("Approximation accuracies: ", accuracy(gm))
+
+# Testing addition and removal of tree constraints.
+
+
+# Solving of model
+status = globalsolve(gm)
+println("X values: ", solution(gm))
+println("Optimal X: ", vcat(exp.([5.01063529, 3.40119660, -0.48450710]), [-147-2/3]))
+
+# Testing constraint addition and removal
+clear_tree_constraints!(gm) # Clears all BBF constraints
+@test all([!is_valid(gm.model, constraint) for constraint in gm.bbfs[2].mi_constraints])
+add_tree_constraints!(gm, [gm.bbfs[2]])
+@test all([is_valid(gm.model, constraint) for constraint in gm.bbfs[2].mi_constraints])
+add_tree_constraints!(gm);
+clear_tree_constraints!(gm, [gm.bbfs[1]])
+@test !any(is_valid(gm.model, constraint) for constraint in gm.bbfs[1].mi_constraints)
+clear_tree_constraints!(gm) # Finds and clears the one remaining BBF constraint.
+@test all([!is_valid(gm.model, constraint) for constraint in gm.bbfs[1].mi_constraints])
+@test all([!is_valid(gm.model, var) for var in gm.bbfs[1].leaf_variables])
