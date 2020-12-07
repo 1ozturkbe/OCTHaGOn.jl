61d12c5fe9c85430b24e634677efae777a11cf17
1ozturkbe <1ozturkbe@gmail.com>
2020-12-07 12:15:51 -0500

    Almost there for scalar functions...

--- test/benchmarks/gams.jl
+++ test/benchmarks/gams.jl
@@ -53,13 +53,13 @@ function is_equality(eq::GAMSFiles.GCall)
 end
 
 
-function find_vars_in_eq(model::JuMP.Model, eq::GAMSFiles.GCall, gams::Dict{String, Any})
+function find_vars_in_eq(eq::GAMSFiles.GCall, gams::Dict{String, Any})
     """Finds and returns all varkeys in equation. """
-    vars = []
+    vars = String[]
     lhs, rhs = eq.args[1], eq.args[2]
     for (var, vinfo) in GAMSFiles.allvars(gams)
         if GAMSFiles.hasvar(lhs, var) || GAMSFiles.hasvar(rhs, var)
-            push!(vars, JuMP.variable_by_name(model, var))
+            push!(vars, var)
         end
     end
     return vars
@@ -68,6 +68,7 @@ end
 """Takes gams and turns them into JuMP.Variables"""
 function generate_variables!(model::JuMP.Model, gams::Dict{String, Any})
     gamsvars = GAMSFiles.allvars(gams)
+    vardict = Dict{String, Any}()
     for (var, vinfo) in gamsvars
         if vinfo isa Array
             sizes = [Base.OneTo(vin) for vin in size(vinfo)]
@@ -78,9 +79,11 @@ function generate_variables!(model::JuMP.Model, gams::Dict{String, Any})
                 set_name(nv[idx], "$(var)[$(join(Tuple(idx),","))]")
                 fix(nv[idx], vinfo[idx])
             end
+            vardict[var] = nv
         elseif vinfo isa Real
             nv = @variable(model, base_name = var)
             fix(nv, vinfo)
+            vardict[var] = nv
         else
             axs = vinfo.axs
             nv = nothing
@@ -93,6 +96,7 @@ function generate_variables!(model::JuMP.Model, gams::Dict{String, Any})
                     set_name(nv[idx], "$(var)[$(join(Tuple(idx),","))]")
                 end
             end
+            vardict[var] = nv
             for (prop, val) in vinfo.assignments
                 inds = map(x->x.val, prop.indices)
                 if isa(prop, Union{GAMSFiles.GText, GAMSFiles.GArray})
@@ -108,65 +112,68 @@ function generate_variables!(model::JuMP.Model, gams::Dict{String, Any})
             end
         end
     end
-    return JuMP.all_variables(model)
-end
-
-""" Converts a GAMS optimization model to a GlobalModel."""
-function GAMS_to_GlobalModel(filename::String)
-    model = JuMP.Model(Gurobi.Optimizer)
-    # Parsing GAMS Files
-    lexed = GAMSFiles.lex(filename)
-    gams = GAMSFiles.parsegams(filename)
-    GAMSFiles.parseconsts!(gams)
-
-    vars = GAMSFiles.getvars(gams["variables"])
-    sets = gams["sets"]
-    preexprs, bodyexprs = Expr[], Expr[]
-    if haskey(gams, "parameters") && haskey(gams, "assignments")
-        GAMSFiles.parseassignments!(preexprs, gams["assignments"], gams["parameters"], sets)
-    end
-
-    # Getting variables
-    all_JuMP_vars = generate_variables!(model, gams)
-
-    # Getting objective
-    if gams["minimizing"] isa String
-        @objective(model, Min, JuMP.variable_by_name(model, gams["minimizing"]))
-    else
-        @objective(model, Min, sum([JuMP.variable_by_name(i) for i in gams["minimizing"]]))
-    end
-
-    # Creating GlobalModel
-    gm = GlobalModel(model = model)
-    equations = [] # For debugging purposes...
-    for (key, eq) in gams["equations"]
-        push!(equations, key => eq)
-    end
-    for (key, eq) in equations
-        if key isa GAMSFiles.GText
-            constr_fn = eq_to_expr(eq, sets)
-            constr_vars = find_vars_in_eq(model, eq, gams)
-            add_nonlinear_constraint(gm, constr_fn, vars = constr_vars,
-                                     equality = is_equality(eq), name = GAMSFiles.getname(key))
-        elseif key isa GAMSFiles.GArray
-            axs = GAMSFiles.getaxes(key.indices, sets)
-            idxs = collect(Base.product([collect(ax) for ax in axs]...))
-            for idx in idxs
-                next_idx = Dict(key.indices[i].text => idxs[i] for i=1:length(key.indices))
-                new_key = string(key.name, "_", idx)
-                new_set = merge(sets, next_idx)
-                constr_fn = eq_to_expr(eq, new_set)
-                constr_vars = find_vars_in_eq(model, eq, gams)
-                add_nonlinear_constraint(gm, constr_fn, vars = constr_vars,
-                                         equality = is_equality(eq),
-                                         name = new_key)
-            end
-        end
-    end
-    return gm
+    return vardict
 end
 
 filename = "data/baron_nc_ns/problem3.13.gms"
-gm = GAMS_to_GlobalModel(filename)
+""" Converts a GAMS optimization model to a GlobalModel."""
+# function GAMS_to_GlobalModel(filename::String)
+model = JuMP.Model(Gurobi.Optimizer)
+# Parsing GAMS Files
+lexed = GAMSFiles.lex(filename)
+gams = GAMSFiles.parsegams(filename)
+GAMSFiles.parseconsts!(gams)
+
+vars = GAMSFiles.getvars(gams["variables"])
+sets = gams["sets"]
+preexprs, bodyexprs = Expr[], Expr[]
+if haskey(gams, "parameters") && haskey(gams, "assignments")
+    GAMSFiles.parseassignments!(preexprs, gams["assignments"], gams["parameters"], sets)
+end
+
+# Getting variables
+vardict = generate_variables!(model, gams)
+
+# Getting objective
+if gams["minimizing"] isa String
+    @objective(model, Min, JuMP.variable_by_name(model, gams["minimizing"]))
+else
+    @objective(model, Min, sum([JuMP.variable_by_name(i) for i in gams["minimizing"]]))
+end
+
+# Creating GlobalModel
+gm = GlobalModel(model = model)
+equations = [] # For debugging purposes...
+for (key, eq) in gams["equations"]
+    push!(equations, key => eq)
+end
+for (key, eq) in equations
+    if key isa GAMSFiles.GText
+        constr_expr = eq_to_expr(eq, sets)
+        constr_varkeys = find_vars_in_eq(eq, gams)
+        input = Symbol.(constr_varkeys)
+        constr_expr = :($(input...) -> $(constr_expr))
+        constr_vars = Array{VariableRef}(flat([vardict[varkey] for varkey in constr_varkeys]))
+        add_nonlinear_constraint(gm, constr_expr, vars = constr_vars,
+                                 equality = is_equality(eq), name = GAMSFiles.getname(key))
+#         elseif key isa GAMSFiles.GArray
+#             axs = GAMSFiles.getaxes(key.indices, sets)
+#             idxs = collect(Base.product([collect(ax) for ax in axs]...))
+#             for idx in idxs
+#                 next_idx = Dict(key.indices[i].text => idxs[i] for i=1:length(key.indices))
+#                 new_key = string(key.name, "_", idx)
+#                 new_set = merge(sets, next_idx)
+#                 constr_fn = eq_to_expr(eq, new_set)
+#                 constr_vars = find_vars_in_eq(model, eq, gams)
+#                 add_nonlinear_constraint(gm, constr_fn, vars = constr_vars,
+#                                          equality = is_equality(eq),
+#                                          name = new_key)
+#             end
+    end
+end
+#     return gm
+# end
+
+# gm = GAMS_to_GlobalModel(filename)
 @test length(gm.vars) == 8
 
