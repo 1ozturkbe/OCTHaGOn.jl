813a57ee4e47672d2c2a9b87e83b401782a5ba21
1ozturkbe <1ozturkbe@gmail.com>
2020-12-10 17:43:12 -0500

    Moving unused gams functions.

--- test/benchmarks/gams.jl
+++ test/benchmarks/gams.jl
@@ -7,30 +7,6 @@ gams:
 
 using GAMSFiles
 
-"""Returns all constants from GAMS Model. """
-function constants(gams::Dict{String, Any})
-    consts = Dict{String, Any}(); #
-    for p in ("parameters", "tables")
-        if haskey(gams, p)
-            for (k, v) in gams[p]
-                v = v isa Ref ? v[] : v
-                consts[GAMSFiles.getname(k)] = v;
-            end
-        end
-    end
-    return consts
-end
-
-""" Moves GAMS constants into the global scope.
-    NOTE: This is only for debugging purposes, could cause bugs.
-"""
-function constants_to_global(gams::Dict{String, Any})
-    consts = constants(gams)
-    for (key, value) in consts
-        eval(Meta.parse("$(key) = $(value)"))
-    end
-end
-
 """ Turns GAMSFiles.GCall into an Expr. """
 function eq_to_expr(eq::GAMSFiles.GCall, sets::Dict{String, Any})
     @assert(length(eq.args)==2)
