8af0ee67ae73056ad0d4ebd25d8b7045fa644116
1ozturkbe <1ozturkbe@gmail.com>
2020-12-29 15:23:19 -0500

    More bugs...

--- src/learners.jl
+++ src/learners.jl
@@ -14,12 +14,12 @@ function base_otr(max_depth::Int64 = 5, minbucket::Float64 = 0.01)
 end
 
 """
-    base_otc(max_depth::Int64 = 5, minbucket::Float64 = 0.01, localsearch::Bool = false)
+    base_otc(localsearch::Bool = false, max_depth::Int64 = 5, minbucket::Float64 = 0.01)
 
 Returns the baseline OptimalTreeClassifier, with parameters for different training steps.
 """
 
-function base_otc(max_depth::Int64 = 5, minbucket::Float64 = 0.01, localsearch::Bool = false)
+function base_otc(localsearch::Bool = false, max_depth::Int64 = 5, minbucket::Float64 = 0.01, )
     return IAI.OptimalTreeClassifier(
         random_seed = 1,
         max_depth = 5,
