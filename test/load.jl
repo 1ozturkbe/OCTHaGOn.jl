92d24d02b6b4779610d96c2eccb56f384e7dcab2
1ozturkbe <1ozturkbe@gmail.com>
2020-12-30 13:45:51 -0500

    learn_constraint!

--- test/load.jl
+++ test/load.jl
@@ -9,7 +9,7 @@ using DataFrames
 using JuMP
 using BARON
 using CPLEX
-using Gurobi
+# using Gurobi
 using Ipopt
 using MathOptInterface
 using Test
@@ -22,7 +22,7 @@ global MOI = MathOptInterface
 global CPLEX_SILENT = with_optimizer(CPLEX.Optimizer, CPX_PARAM_SCRIND = 0)
 global IPOPT_SILENT = with_optimizer(Ipopt.Optimizer, print_level = 0)
 global BARON_SILENT = with_optimizer(BARON.Optimizer, OutputFlag = 0)
-global CPLEX_SILENT = with_optimizer(Gurobi.Optimizer, OutputFlag = 0, Gurobi.Env())
+# global GUROBI_SILENT = with_optimizer(Gurobi.Optimizer, OutputFlag = 0, Gurobi.Env())
 Random.seed!(1);
 MOI.Silent() = true;
 
