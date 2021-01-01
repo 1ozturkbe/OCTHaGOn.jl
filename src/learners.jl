989193060f03c9f4c81a47ad086d6d214dc93046
1ozturkbe <1ozturkbe@gmail.com>
2021-01-01 16:16:01 -0500

    Learners modifiable by kwargs.

--- src/learners.jl
+++ src/learners.jl
@@ -1,33 +1,39 @@
 """ Returns the baseline OptimalTreeRegressor. """
 function base_otr(; max_depth::Int64 = 5, minbucket::Float64 = 0.01)
     return IAI.OptimalTreeRegressor(
-        random_seed = 1,
-        max_depth = 3,
-        cp = 1e-6,
-        minbucket = 0.03,
-        regression_sparsity = :all,
-        fast_num_support_restarts = 5,
         hyperplane_config = (sparsity = :all,),
-        regression_lambda = 0.00001,
+        regression_sparsity = :all,
         regression_weighted_betas = true,
+        # Modifiables
+        random_seed = get(kwargs, :random_seed, 1),
+        max_depth = get(kwargs, :max_depth, 5),
+        cp = get(kwargs, :cp, 1e-6),
+        minbucket = get(kwargs, :minbucket, 0.01),
+        fast_num_support_restarts = get(kwargs, :fast_num_support_restarts, 5),
+        regression_lambda = get(kwargs, :regression_lambda, 1e-5),
+        ls_num_tree_restarts = get(kwargs, :ls_num_tree_restarts, 10),
+        ls_num_hyper_restarts =  get(kwargs, :ls_num_hyper_restarts, 5),
     )
 end
 
 """
-    base_otc(localsearch::Bool = true, max_depth::Int64 = 5, minbucket::Float64 = 0.01)
+    base_otc(;kwargs...)
 
 Returns the baseline OptimalTreeClassifier, with parameters for different training steps.
 """
 
-function base_otc(; localsearch::Bool = true, max_depth::Int64 = 5, minbucket::Float64 = 0.01)
-    return IAI.OptimalTreeClassifier(
-        random_seed = 1,
-        max_depth = 5,
-        cp = 1e-6,
-        minbucket = minbucket,
-        fast_num_support_restarts = 5,
+function ase_otc(; kwargs...)
+    lnr = IAI.OptimalTreeClassifier(
         hyperplane_config = (sparsity = :all,),
-        localsearch = localsearch,
+        # Modifiables
+        random_seed = get(kwargs, :random_seed, 1),
+        max_depth = get(kwargs, :max_depth, 5),
+        cp = get(kwargs, :cp, 1e-6),
+        minbucket = get(kwargs, :minbucket, 0.01),
+        fast_num_support_restarts = get(kwargs, :fast_num_support_restarts, 5),
+        localsearch = get(kwargs, :localsearch, true),
+        ls_num_tree_restarts = get(kwargs, :ls_num_tree_restarts, 10),
+        ls_num_hyper_restarts =  get(kwargs, :ls_num_hyper_restarts, 5),
     )
 end
 
