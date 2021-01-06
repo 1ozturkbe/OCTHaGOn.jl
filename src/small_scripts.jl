0540e4bc879e1de69ee3170bf7e81b656b143c8a
1ozturkbe <1ozturkbe@gmail.com>
2021-01-06 13:27:27 -0500

    Moving helper funcions from bbf to small_scripts.

--- src/small_scripts.jl
+++ src/small_scripts.jl
@@ -45,3 +45,90 @@ function check_bounds(bounds::Dict)
         return
     end
 end
+
+"""
+    get_varmap(expr_vars::Array, vars::Array)
+
+Helper function to map vars to flatvars.
+Arguments:
+    flatvars is a flattened Array{JuMP.VariableRef}
+    vars is the unflattened version, usually derived from an Expr.
+Returns:
+    Dict of ID maps
+"""
+function get_varmap(expr_vars::Array, vars::Array)
+    length(flat(expr_vars)) >= length(vars) || throw(OCTException(string("Insufficiently many input
+        variables declared in ", vars, ".")))
+    unique(vars) == vars || throw(OCTException(string("Nonunique variables among ", vars, ".")))
+    varmap = Tuple[(0,0) for i=1:length(vars)]
+    for i = 1:length(expr_vars)
+        if expr_vars[i] isa JuMP.VariableRef
+            try
+                varmap[findall(x -> x == expr_vars[i], vars)[1]] = (i,0)
+            catch
+                throw(OCTException(string("Scalar variable ", expr_vars[i], " was not properly declared in vars.")))
+            end
+        else
+            for j=1:length(expr_vars[i])
+                try
+                    varmap[findall(x -> x == expr_vars[i][j], vars)[1]] = (i,j)
+                catch
+                    continue
+                end
+            end
+        end
+    end
+    length(varmap) == length(vars) || throw(OCTException(string("Could not properly map
+                                            expr_vars: ", expr_vars,
+                                            "to vars: ", vars, ".")))
+    return varmap
+end
+
+get_varmap(expr_vars::Nothing, vars::Array) = nothing
+
+"""
+    infarray(varmap::Array)
+
+Creates a template array for deconstruct function.
+"""
+function infarray(varmap::Array)
+    arr = []
+    # Pre allocate and fill!
+    copied_map = sort(deepcopy(varmap))
+    firsts = first.(copied_map)
+    seconds = last.(copied_map)
+    arr_length = length(unique(firsts))
+    for i=1:arr_length
+        tup_idx = findall(j -> j[1] == i, copied_map)
+        if seconds[tup_idx] == [0]
+            push!(arr, Inf)
+        else
+            push!(arr, Inf*ones(maximum(seconds[tup_idx])))
+        end
+    end
+    return arr
+end
+
+"""
+    deconstruct(data::DataFrame, vars::Array, varmap::Array)
+
+Takes in data for input into a Function, and rips it apart into appropriate arrays.
+"""
+function deconstruct(data::DataFrame, vars::Array, varmap::Array)
+    n_samples, n_vars = size(data)
+    infarr = infarray(varmap)
+    arrs = [];
+    stringvars = string.(vars)
+    for i = 1:n_samples
+        narr = deepcopy(infarr)
+        for j = 1:length(varmap)
+            if varmap[j] isa Tuple && varmap[j][2] != 0
+                narr[varmap[j][1]][varmap[j][2]] = data[i, stringvars[j]]
+            else
+                narr[varmap[j][1]] = data[i, stringvars[j]]
+            end
+        end
+        push!(arrs, narr)
+    end
+    return arrs
+end
