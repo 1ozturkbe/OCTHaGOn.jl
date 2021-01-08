e1853f6a38e42f6c979df5f052fbd88fbfd5f9e5
1ozturkbe <1ozturkbe@gmail.com>
2021-01-07 20:08:08 -0500

    nlp3.jl

--- data/baron/nlp3.jl
+++ data/baron/nlp3.jl
@@ -1,49 +1,51 @@
-POSITIVE_VARIABLES x1, x2, x3, x4, x5, x6, x7, x8, x9, x10;
-LOWER_BOUNDS{
-x1: 1;
-x2: 1;
-x4: 1;
-x6: 85;
-x7: 90;
-x8: 3;
-x9: 1.2;
-x10: 145;
-}
-UPPER_BOUNDS{
-x1: 2000;
-x2: 16000;
-x3: 120;
-x4: 5000;
-x5: 2000;
-x6: 93;
-x7: 95;
-x8: 12;
-x9: 4;
-x10: 162;
-}
-EQUATIONS  e1, e2, e3, e4, e5, e6, e7;
-
-e1: x1 - 1.22*x4 + x5 == 0;
-e2: x9 + 0.222*x10 == 35.82;
-e3: 3*x7 - x10 == 133;
-//e4: x7 + 0.002*x7*log(x7) - 1.098*x8 + 0.038*(x8^2) - 0.325*(x6 - 89) == 86.35;
-e4: x7 - 1.098*x8 + 0.038*(x8^2) - 0.325*(x6 - 89) == 86.35;
-e5: x4*x9*x6 + 1000*x3*x6 - 98000*x3 == 0;
-e6: x2 + x5 - x1*x8 == 0;
-e7: 1.12*x1 + 0.13167*x8*x1 - 0.00667*(x8^2)*x1 - x4 >= 0;
-OBJ: minimize 5.04*x1 + 0.035*x2 + 10*x3 + 3.36*x5 - 0.063*x4*x7;
-//OBJ: maximize -5.04*x1 - 0.035*x2 - 10*x3 - 3.36*x5 + 0.063*x4*x7;
-
-STARTING_POINT{
-x1: 1724.90452208;
-x2: 16000;
-x3: 98.0900813608;
-x4: 3049.1211364;
-x5: 1995.02326433;
-x6: 90.718089075;
-x7: 94.2274481766;
-x8: 10.432474977;
-x9: 2.59051951438;
-x10: 149.682344530;
-}
+function nlp3(gm::Bool = false)
+    m = JuMP.Model()
+    @variable(m, 0 <= x[1:10])
+    for i in [1, 2, 4, 6, 7, 8, 9, 10]
+        JuMP.set_lower_bound(x[i], [1,1,1,85,90,3,1.2,145][i])
+    end
+    for i = 1:10
+        JuMP.set_upper_bound(x[i], [2000, 16000, 120, 5000, 2000,
+                                    93, 95, 12, 4, 162][i])
+        JuMP.set_start_value(x[i], [1724.90452208, 16000,
+                                    98.0900813608, 3049.1211364,
+                                    1995.02326433, 90.718089075,
+                                    94.2274481766, 10.432474977,
+                                    2.59051951438, 149.682344530][i])
+    end
+    if !gm
+        set_optimizer(m, BARON_SILENT)
+        @NLconstraint(m, e1, x[1] - 1.22*x[4] + x[5] == 0)
+        @NLconstraint(m, e2, x[9] + 0.222*x[10] == 35.82)    
+        @NLconstraint(m, e3, 3*x[7] - x[10] == 133)    
+        @NLconstraint(m, e4, x7 - 1.098*x8 + 0.038*(x8^2) - 0.325*(x6 - 89) == 86.35)    
+        @NLconstraint(m, e5, x[4]*x[9]*x[6] + 1000*x[3]*x[6] - 98000*x[3] == 0)    
+        @NLconstraint(m, e6, x[2] + x[5] - x[1]*x[8] == 0)    
+        @NLconstraint(m, e7, 1.12*x[1] + 0.13167*x[8]*x[1] - 0.00667*(x[8]^2)*x[1] - x[4] >= 0)    
+        @NLobj(m, Min, 5.04*x[1] + 0.035*x[2] + 10*x[3] + 3.36*x[5] - 0.063*x[4]*x[7])        
+    else
+        @variable(m, obj)
+        @objective(m, Min, obj)
+        gm = GlobalModel(model = m, name = "nlp3")
+        set_optimizer(gm, CPLEX_SILENT)
+        add_nonlinear_constraint(gm, :(x -> x[1] - 1.22*x[4] + x[5]), 
+                                vars = [x[1], x[4], x[5]], name = "e1", equality = true)
+        add_nonlinear_constraint(gm, :(x[9] + 0.222*x[10] - 35.82), 
+                                vars = [x[9], x[10]], name = "e2", equality = true)
+        add_nonlinear_constraint(gm, :(x -> 3*x[7] - x[10] - 133), 
+                                vars = [x[7], x[10]], name = "e3", equality = true)
+        add_nonlinear_constraint(gm, :(x -> x[7] - 1.098*x[8] + 0.038*(x[8]^2) - 0.325*(x[6] - 89) - 86.35), 
+                                vars = [x[6], x[7], x[8]], name = "e4", equality = true)
+        add_nonlinear_constraint(gm, :(x -> x[4]*x[9]*x[6] + 1000*x[3]*x[6] - 98000*x[3]), 
+                                vars = [x[3], x[4], x[6], x[9]], name = "e5", equality = true)
+        add_nonlinear_constraint(gm, :(x -> x[2] + x[5] - x[1]*x[8]), 
+                                vars = [x[1], x[2], x[5], x[8]], name = "e6", equality = true)
+        add_nonlinear_constraint(gm, :(x -> 1.12*x[1] + 0.13167*x[8]*x[1] - 0.00667*(x[8]^2)*x[1] - x[4]), 
+                                vars = [x[1], x[4], x[8]], name = "e7")
+        add_nonlinear_constraint(gm, :(x -> 5.04*x[1] + 0.035*x[2] + 10*x[3] + 3.36*x[5] - 0.063*x[4]*x[7]), 
+                                vars = [x[1], x[2], x[3], x[4], x[5], x[7]], 
+                                dependent_var = obj, name = "obj")
+    end
+    return m
+end
 
