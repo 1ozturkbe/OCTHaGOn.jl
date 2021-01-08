b6fe5e55e38cc54cc5f14d476c969405bb5a7316
1ozturkbe <1ozturkbe@gmail.com>
2021-01-08 18:33:28 -0500

    Committing changes before I break something.

--- test/benchmarks/baron.jl
+++ test/benchmarks/baron.jl
@@ -1,18 +1,37 @@
 include("../load.jl")
 
+# function recipe(gm::GlobalModel)
+#     t = [time()]
+#     println("GlobalModel " * gm.name * " in progress...")
+#     set_optimizer(gm, CPLEX_SILENT)
+#     find_bounds!(gm, all_bounds=true)
+#     set_param(gm, :ignore_feasibility, true)
+#     set_param(gm, :ignore_accuracy, true)
+#     push!(t, time())
+#     uniform_sample_and_eval!(gm)
+#     push!(t, time())
+#     println("Sampling time: $(t[end])")
+#     println("Sample feasibilities ", feasibility(gm))
+#     learn_constraint!(gm)
+#     push!(t, time())
+#     print_ln("Approximation accuracies: ", accuracy(gm))
+#     save_fit(gm)
+#     push!(t, time())
+#     globalsolve(gm)
+#     push!(t, time())
+#     return gm
+# end
+
 function recipe(gm::GlobalModel)
     @info "GlobalModel " * gm.name * " in progress..."
     set_optimizer(gm, CPLEX_SILENT)
-    find_bounds!(gm, all_bounds=true)
+    find_bounds!(gm, all_bounds=false)
     set_param(gm, :ignore_feasibility, true)
     set_param(gm, :ignore_accuracy, true)
     uniform_sample_and_eval!(gm)
-    @info ("Sample feasibilities ", feasibility(gm))
     learn_constraint!(gm)
-    @info("Approximation accuracies: ", accuracy(gm))
     save_fit(gm)
     globalsolve(gm)
-    return
 end
 
 # Loading BARON examples (that haven't been loaded yet)
@@ -20,20 +39,21 @@ for i in ["nlp1.jl", "nlp2.jl", "nlp3.jl"]
     include(OCT.BARON_DIR * i)
 end
 
-
 BARON_PROBLEMS = [gear, minlp, nlp1, nlp2, nlp3]
 
-bms = [p(false) for p in BARON_PROBLEMS]
-gms = [p(true) for p in BARON_PROBLEMS]
-
-for bm in bms
-    optimize!(bm);
-end
-
-for gm in gms
-    try
-        uniform_sample_and_eval!(gm, lh_iterations=5)
-    catch
-        println("$(gm.name): " * string(get_unbounds(gm)))
+function optimize_and_time!(m::Union{JuMP.Model, GlobalModel})
+    t1 = time()
+    if m isa JuMP.Model
+        optimize!(m);
+    else
+        recipe(m)
     end
+    println("Model solution time: " * string(time()-t1) * " seconds.")
 end
+
+bms = [p(false) for p in BARON_PROBLEMS]
+optimize_and_time!.(bms)
+
+
+gms = [p(true) for p in BARON_PROBLEMS]
+optimize_and_time!.(gms)
