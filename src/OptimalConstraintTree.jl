5cccd754b1edf979e3a6ae492f1dcf314e1dd4a4
1ozturkbe <1ozturkbe@gmail.com>
2021-01-13 10:33:11 -0500

    Adding ridge_ and ul_regress.

--- src/OptimalConstraintTree.jl
+++ src/OptimalConstraintTree.jl
@@ -51,54 +51,53 @@ module OptimalConstraintTree
     include("optimize.jl")
 
     include("plot.jl")
-
-           # Structs
-    export GlobalModel,
-           BlackBoxLearner, BlackBoxClassifier, BlackBoxRegressor,
-           # GlobalModel Functions
-           globalsolve,
-           # JuMP.Model extensions to GlobalModel
-           set_optimizer, optimize!,
-           # Functions on GlobalModel and BlackBoxLearners
-           learn_from_data!, find_bounds!, find_linear_bounds!,
-           add_nonlinear_constraint, add_nonlinear_or_compatible, determine_vars,
-           lh_sample, boundary_sample,
-           accuracy, feasibility, check_accuracy, check_feasibility, 
-           check_bounds,  check_sampled, 
-           solution, 
-           evaluate_feasibility,
-           nonlinearize!,
-           save_fit, load_fit, set_param, get_param, 
-           # Functions on BlackBoxLearners
-           show_trees, learn_constraint!,
-           # Functions on BlackBoxLearners only
-           eval!, uniform_sample_and_eval!,
-           secant_method, knn_sample, build_knn_tree,
-           find_knn, classify_patches,
-           # Functions on BlackBoxLearners
-           add_data!, match_bbls_to_vars,
-           # Functions on IAI structs
-           find_leaves, regress, check_if_trained, trust_region_data, pwl_constraint_data,
-           # Functions on JuMP objects
-           evaluate, fetch_variable, get_bounds, get_unbounds,
-           linearize_objective!, classify_constraints,
-           bound!,
-           data_to_DataFrame, data_to_Dict,
-           distance_to_set, get_constant,
-           add_feas_constraints!, add_regr_constraints!,
-           add_tree_constraints!, clear_tree_constraints!,
-           base_regressor, base_classifier,
-           fit_regressor_kwargs, fit_classifier_kwargs, 
-           regressor_kwargs, classifier_kwargs,
-           functionify,
-           # Functions to import global optimization problems,
-           sagemark_to_GlobalModel,
-           alphac_to_expr, alphac_to_objexpr, alphac_to_varbound!,
-           # Exceptions
-           OCTException,
-           # Display and plotting
-#            plot_2d, plot_2d_predictions, plot_accuracies
-           # Debugging tools
+        
+        # Structs
+    export GlobalModel, BlackBoxLearner, BlackBoxClassifier, BlackBoxRegressor,
+        # GlobalModel Functions
+        globalsolve,
+        # JuMP.Model extensions to GlobalModel
+        set_optimizer, optimize!,
+        # Functions on GlobalModel and BlackBoxLearners
+        learn_from_data!, find_bounds!, find_linear_bounds!,
+        add_nonlinear_constraint, add_nonlinear_or_compatible, determine_vars,
+        lh_sample, boundary_sample,
+        accuracy, feasibility, check_accuracy, check_feasibility, 
+        check_bounds,  check_sampled, 
+        solution, 
+        evaluate_feasibility,
+        nonlinearize!,
+        save_fit, load_fit, set_param, get_param, 
+        # Functions on BlackBoxLearners
+        show_trees, learn_constraint!,
+        # Functions on BlackBoxLearners only
+        eval!, uniform_sample_and_eval!,
+        secant_method, knn_sample, build_knn_tree,
+        find_knn, classify_patches,
+        # Functions on BlackBoxLearners
+        add_data!, match_bbls_to_vars,
+        # Functions on IAI structs
+        find_leaves, regress, check_if_trained, trust_region_data, pwl_constraint_data,
+        # Functions on JuMP objects
+        evaluate, fetch_variable, get_bounds, get_unbounds,
+        linearize_objective!, classify_constraints,
+        bound!,
+        data_to_DataFrame, data_to_Dict,
+        distance_to_set, get_constant,
+        add_feas_constraints!, add_regr_constraints!,
+        add_tree_constraints!, clear_tree_constraints!,
+        base_regressor, base_classifier,
+        fit_regressor_kwargs, fit_classifier_kwargs, 
+        regressor_kwargs, classifier_kwargs,
+        functionify,
+        # Other approximators, 
+        ridge_regress, ul_regress,
+        # Functions to import global optimization problems,
+        sagemark_to_GlobalModel,
+        alphac_to_expr, alphac_to_objexpr, alphac_to_varbound!,
+        # Exceptions
+        OCTException,
+        # Debugging tools
             clear_data!,
             # Small scripts
             vars_from_expr, get_varmap, deconstruct, flat,
