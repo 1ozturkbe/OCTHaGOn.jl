bb76276dd1dfab0f2da10f591c82dd61bbb60aea
1ozturkbe <1ozturkbe@gmail.com>
2021-01-21 18:02:30 -0500

    QP in a good enough place for experiments.

--- test/benchmarks/qp.jl
+++ test/benchmarks/qp.jl
@@ -4,13 +4,13 @@ using StatsBase
 function random_qp(dims::Int64, nconstrs::Int64, sparsity=dims, solver = CPLEX_SILENT)
     m = JuMP.Model(with_optimizer(solver))
     @variable(m, -1 <= x[1:dims] <= 1)
-    sparse_vars = randperm(dims)[1:sparsity]
+    m[:sparse_vars] = randperm(dims)[1:sparsity]
     m[:A] = rand(dims, sparsity).*2 .- 1
     m[:b] = rand(dims)
     m[:C] = rand(nconstrs, dims).*2 .- 1
     m[:d] = rand(nconstrs)
     @constraint(m, m[:C]*x .>= m[:d])
-    @objective(m, Min, sum((m[:A]*x[sparse_vars] - m[:b]).^2))
+    @objective(m, Min, sum((m[:A]*x[m[:sparse_vars]] - m[:b]).^2))
     return m
 end
 
@@ -18,7 +18,7 @@ function gmify(m::JuMP.Model)
     @variable(m, obj)
     @objective(m, Min, obj)
     gm = GlobalModel(model = m)
-    add_nonlinear_constraint(gm, :(x -> sum(($(m[:A])*x[$(sparse_vars)] - $(m[:b])).^2)), vars = m[:x][sparse_vars], 
+    add_nonlinear_constraint(gm, :(x -> sum(($(m[:A])*x[$(m[:sparse_vars])] - $(m[:b])).^2)), vars = m[:x][m[:sparse_vars]], 
                                     dependent_var = obj, name = "qp") 
     return gm
 end
@@ -55,12 +55,12 @@ end
 
 Binary knn samples from current leaf to surrounding leaves.   
 """
-function knn_outward_from_leaf(bbl::BlackBoxLearner, leaf_in::Int64 = find_leaf_of_soln(bbl))
+function knn_outward_from_leaf(bbl::BlackBoxLearner, leaf_in::Int64 = find_leaf_of_soln(bbl), k = 10)
     leaves = IAI.apply(bbl.learners[end], bbl.X);
     leaf_neighbors = findall(x -> x == leaf_in, leaves);
-    df = DataFrame([Float64 for i in vks], vks)
+    df = DataFrame([Float64 for i in string.(bbl.vars)], string.(bbl.vars))
     build_knn_tree(bbl);
-    idxs, dists = find_knn(bbl, k=k);
+    idxs, dists = find_knn(bbl, k = k);
     leaf_neighbors = findall(x -> x == leaf_in, leaves)
     leaf_neighbors = leaf_neighbors[randperm(MersenneTwister(1234), length(leaf_neighbors))]; # Shuffling pre while loop
     ct = 1
@@ -93,11 +93,14 @@ uniform_sample_and_eval!(gm)
 uppers = []
 lowers = []
 actuals = []
+thresholds = []
+tightnesses = []
 threshold = quantile(bbl.Y, 0.25)
-learn_constraint!(gm, threshold=threshold)
+push!(thresholds, threshold)
+
+learn_constraint!(gm, threshold=thresholds[end])
 add_tree_constraints!(gm)
 optimize!(gm)
-
 
 
 leaf_in = find_leaf_of_soln(bbl)
@@ -112,12 +115,14 @@ eval!(bbl, df)
 
 # Do something about uppers, lowers and actuals. 
 if actuals[end] <= lowers[end]
-    threshold = lowers[end]
+    push!(thresholds, lowers[end])
 elseif lowers[end] <= actuals[end] <= uppers[end]
-    threshold = (uppers[end] - actuals[end])/2 + actuals[end]
+    push!(thresholds[end], (uppers[end] - actuals[end])/2 + actuals[end])
 elseif actuals[end] >= uppers[end]
-    threshold = threshold
+    push!(thresholds, actuals[end])
     push!(bbl.learners, bbl.learners[end])
     push!(bbl.ul_data, ul_boundify(bbl.learners[end], bbl.X, bbl.Y))
 end
 
+push!(tightnesses, abs(lowers[end] - actuals[end]) /(maximum(bbl.Y) - minimum(bbl.Y)))
+clear_tree_constraints!(gm)
