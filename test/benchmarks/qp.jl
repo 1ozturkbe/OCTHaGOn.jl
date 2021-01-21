ea7a4920e147e1ca59f907f9b1ee0b6377eb6a65
1ozturkbe <1ozturkbe@gmail.com>
2021-01-21 16:54:24 -0500

    Lots of new things I shouldn't mix up...

--- test/benchmarks/qp.jl
+++ test/benchmarks/qp.jl
@@ -41,12 +41,20 @@ threshold = quantile(bbl.Y, 0.25)
 learn_constraint!(gm, threshold=threshold)
 add_tree_constraints!(gm)
 optimize!(gm)
-# Find the leaf we are in via binary variables
-leaf_in = 0
-for (leaf, var) in bbl.leaf_variables
-    if getvalue(var) == 1
-        leaf_in = leaf
+
+""" 
+    find_leaf_of_soln(bbl::BlackBoxLearner)
+
+Find leaf of previous solution via binary variables. 
+"""
+function find_leaf_of_soln(bbl::BlackBoxLearner)
+    leaf_in = 0
+    for (leaf, var) in bbl.leaf_variables
+        if getvalue(var) == 1
+            leaf_in = leaf
     end
+    @assert leaf_in != 0
+    return leaf_in
 end
 @assert leaf_in != 0
 #UL_data for the leaf
@@ -55,9 +63,84 @@ push!(uppers, α0 + sum(α .* getvalue.(bbl.vars)))
 push!(lowers, β0 + sum(β .* getvalue.(bbl.vars)))
 push!(actual, bbl(solution(gm)))
 
-if actual[end] >= 
+# Getting new data
 
-leaf = getvalue.(collect(bbl.leaf_variables))
+leaf_neighbors = findall(x -> x == leaves[end], leaves);
+df = DataFrame([Float64 for i in vks], vks)
+build_knn_tree(bbl);
+idxs, dists = find_knn(bbl, k=k);
+leaves = IAI.apply(bbl.learners[end], bbl.X);
+leaf_neighbors = randperm(MersenneTwister(1234), findall(x -> x == leaf_in, leaves));
+count = 1
+while size(df, 1) <= get_param(bbl, :n_samples)/2
+    signs = [leaves[i] == leaf_in for i in idxs[count]]
+    if length(signs) > sum(signs) > 0 
+        inleaf = idxs[count][findall(x -> x == 1, signs)]
+        outleaf = idxs[count][findall(x -> x != 1, signs)]
+        for i in inleaf
+            for j in outleaf
+                np = (Array(bbl.X[i, :]) + Array(bbl.X[j, :]))./2
+                push!(df, np)
+            end
+        end
+    end
+    count += 1
+end
+end
+
+function knn_outward(bbl::BlackBoxClassifier, leaf, leaf_neighbors::Array; k::Int64 = 10, tighttol = 1e-5)
+    vks = string.(bbl.vars)
+    df = DataFrame([Float64 for i in vks], vks)
+    build_knn_tree(bbl);
+    idxs, dists = find_knn(bbl, k=k);
+    feas_class = classify_patches(bbl, idxs);
+    negatives = findall(x -> x .< 0, bbl.Y) # TODO: improve sign checking. 
+    if !isnothing(sample_idxs)
+        negatives = intersect(negatives, sample_idxs)
+    end
+    for i = 1:length(negatives) # This loop is for making sure that every possible root is sampled only once.
+        if feas_class[negatives[i]] == "mixed"
+            nodes = [idxs[negatives[i]][j] for j=1:length(idxs[negatives[i]]) 
+                            if (bbl.Y[idxs[negatives[i]][j]] >= 0 && dists[negatives[i]][j] >= tighttol)]
+            push!(nodes, negatives[i])
+            np = secant_method(bbl.X[nodes, :], bbl.Y[nodes, :])
+            append!(df, np)
+        end
+    end
+    return df
+end
+
+
+            # df = knn_sample(bbl, k = length(bbl.vars), tighttol = get_param(gm, :tighttol), sample_idxs = leaf_neighbors)
+            # last_sample = bbl.knn_tree.data[end]
+            # println("New_samples: ", size(df))
+            # if size(df, 1) > 0
+            #     eval!(bbl, df)
+            # #     weights = reweight(normalized_data(bbl))
+            # #     learn_constraint!(bbl, sample_weight = weights)
+            #     clear_tree_constraints!(gm, bbl)
+            #     add_tree_constraints!(gm, bbl)
+            # #     scatter!(df[:, string(vars[1])], df[:, string(vars[2])], color = :red)
+            # end
+            # LEARN TREE ON DF AS WELL AS ORIGINAL DATA IN THE LEAF. 
+            # REMOVE ORIGINAL CONSTRAINT, RETRAIN WITH THE NEW DATA
+            # weights = reweight(normalized_data(bbl), mag)
+            # Learn greedy SVM approx locally
+            # samp = 30
+            # idxs, dists = OCT.knn(bbl.knn_tree, [last_sample], samp)
+            # β0, β = svm(Matrix(bbl.X[idxs[1], :]), bbl.Y[idxs[1]])
+            leaf_var = bbl.leaf_variables[leaves[end]]
+            M = 1e5
+            gfn = x -> ForwardDiff.gradient(bbl.f, x)
+            varmap = get_varmap(bbl.expr_vars, bbl.vars)        
+            inp = deconstruct(solution(gm), bbl.vars, varmap)
+            grad = gfn(inp[1]...)    
+            grad = [grad[v[2]] for v in varmap]
+            constr = @constraint(gm.model, 0 <= sum(grad .* bbl.vars) + M * (1 - leaf_var))
+            push!(bbl.mi_constraints, constr)
+        end
+
+
 
 
 gmsol = getvalue.(gm.model[:x])
