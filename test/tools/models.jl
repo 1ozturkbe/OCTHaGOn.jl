5bfb992bf0cc902230a5c0f2cd030f57c1538b51
1ozturkbe <1ozturkbe@gmail.com>
2021-01-25 14:55:25 -0500

    Adding basic qp and test_gqp for testing.

--- test/tools/models.jl
+++ test/tools/models.jl
@@ -11,3 +11,24 @@ function test_model()
     JuMP.set_upper_bound(a[1,1], 3)
     return model, x, y, z, a
 end
+
+""" Basic qp for testing. """
+function test_qp(solver = CPLEX_SILENT)
+    m = JuMP.Model(with_optimizer(solver))
+    @variable(m, 2 >= x[1:2] >= 0) 
+    @objective(m, Min, 3*x[1]^2 + x[2]^2 + 2*x[1]*x[2] + x[1] + 6*x[2] + 2)
+    @constraint(m, 2*x[1] + 3*x[2] >= 4)
+    return m
+end
+
+""" Basic gqp, same as above. """
+function test_gqp(solver = CPLEX_SILENT)
+    m = JuMP.Model(with_optimizer(solver))
+    @variable(m, 2 >= x[1:2] >= 0) 
+    @constraint(m, 2*x[1] + 3*x[2] >= 4)
+    @variable(m, obj)
+    @objective(m, Min, obj)
+    gm = GlobalModel(model = m)
+    add_nonlinear_constraint(gm, :(x -> 3*x[1]^2 + x[2]^2 + 2*x[1]*x[2] + x[1] + 6*x[2] + 2), dependent_var = obj)
+    return gm
+end
