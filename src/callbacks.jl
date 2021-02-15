0690a04fd6be673b0540009b8b3d1be3e75b6afd
Berk Ozturk <1ozturkbe@gmail.com>
2021-02-15 14:22:08 -0500

    For future potential callbacks?

---
+++ src/callbacks.jl
@@ -0,0 +1,22 @@
+# """
+# Creates a callback function for the GlobalModel.
+# """
+# function gm_user_cuts(gm::GlobalModel)
+#     M=1e5
+#     for bbl in gm.bbls
+#         if bbl isa BlackBoxClassifier & !bbl.equality
+#             varvals = DataFrame(JuMP.callback_value(cb_data, bbl.vars), string.(bbl.vars))
+#             eval!(bbl, val_vals)
+#             if bbl.Y[end] <= -1*get_param(gm, :tighttol)
+#                 update_gradients(bbl, [size(bbl.X, 1)])
+#                 leaf = IAI.apply(bbl.learners[end], varvals)
+#                 leaf_var = bbl.leaf_variables[leaf[1]]
+#                 con = JuMP.build_constraint(sum(Array(bbl.gradients[end]) .* (bbl.vars .- Array(var_vals)))  + 
+#                                             bbl.Y[end] + M * (1 - leaf_var) >= 0)
+#                 push!(bbl.mi_constraints[leaf[1]], con)
+#                 MOI.submit(gm.model, MOI.LazyConstraint(cb_data), con)
+#             end
+#         end
+#     end
+#     return
+# end
