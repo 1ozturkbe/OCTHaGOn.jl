aa696a537b8fa86714675ee0d6d0dab410f18f75
Berk Ozturk <1ozturkbe@gmail.com>
2021-04-22 18:56:02 -0400

    Still having issues with true anomaly computations.

--- data/oos.jl
+++ data/oos.jl
@@ -18,7 +18,8 @@ alt_sat = 780e3 # altitude of satellites (m)
 r_sat = rE + alt_sat
 period_sat = 2pi*sqrt(r_sat^3/mu)
 
-potential_orbits = rE .+ 1e3 .* collect(680:40:880)  
+potential_orbits = rE .+ 1e3 .* collect(680:10:880)
+potential_orbits = filter!(r -> r != r_sat, potential_orbits)  
 n_orbits = length(potential_orbits)
 orbital_periods = [2pi*sqrt(r^3/mu) for r in potential_orbits]
 dmass_entry = []
@@ -28,7 +29,7 @@ for r in potential_orbits
         push!(dmass_entry, 1/exp(1/(g*Isp) * sqrt(mu/r)*(sqrt(2r_sat/(r_sat + r)) -1)))
         push!(dmass_exit, 1/exp(1/(g*Isp) * sqrt(mu/r_sat)*(1 - sqrt(2r/(r_sat + r)))))
     else
-        push!(dmass_entry, exp(1/(g*Isp) * sqrt(mu/r)*(sqrt(2r_sat/(r_sat + r)) -1)))
+        push!(dmass_entry, exp(1/(g*Isp) * sqrt(mu/r)*(sqrt(2r_sat/(r_sat + r)) -1))) #TODO Check these, don't make sense.
         push!(dmass_exit, exp(1/(g*Isp) * sqrt(mu/r_sat)*(1 - sqrt(2r/(r_sat + r)))))
     end
 end
@@ -40,14 +41,39 @@ m = Model(Gurobi.Optimizer)
 set_optimizer_attribute(m, "NonConvex", 2)
 
 # Transfers xx
-max_idx = findall(x -> x == maximum(fuel_required), fuel_required)
+# Always start by fueling the satellite that needs most fuel
+max_idx = findall(x -> x == maximum(fuel_required), fuel_required) 
+@variable(m, sat_order[i=1:n])
 @variable(m, xx[1:n-1, 1:n_sats], Bin)        # transfer occurs from row index to column index
+
+@constraint(m, sat_order[1] == max_idx[1])
+@constraint(m, [i=2:n], sat_order[i] == sum(xx[i-1, :] .* collect(1:n)))
 @constraint(m, [i=1:n-1], xx[i, i] == 0)      # Cannot self transfer
 @constraint(m, [i=1:n-1], sum(xx[i, :]) == 1) # Exactly one transfer every time (same as each satellite visited once)
 @constraint(m, [i=1:n], sum(xx[:, i]) <= 1)   # At most one transfer to each satellite
 @constraint(m, sum(xx[:, max_idx[1]]) == 0)
-@variable(m, pi >= ta[1:n-1] >= -pi)            # true anomalies
-@constraint(m, [i=1:n-1], ta[i] == sum(circshift(true_anomalies, i-1) .* xx[i, :])) #TODO fix true anomaly
+
+# # True anomaly computation from satellite order
+@variable(m, ta[1:n-1])
+# @variable(m, tapos[1:n-1], Bin) # whether or not true anomaly is positive. 
+# @constraint(m, [i=1:n-1], tapos)
+
+# @variable(m, tax[1:n-1, 1:n], Bin)    
+# @constraint(m, [i=1:n-1], ta[i] == sum(tax[i,:] .* true_anomalies))
+# positive_shifts = collect(1:Int((n_sats-1)/2))
+# for j=1:n
+#     if j-max_idx[1] in positive_shifts || j-max_idx < -n_sats/2
+#         @constraint(m, tax[1,j] <= )
+# end
+# for i=2:n-1
+#     for j=1:n
+#         if j-i in positive_shifts || j-i < -n_sats/2
+#             @constraint(m, tax[i,j] <= )
+# end
+
+@constraint(m, ta[1] == true_anomalies[max_idx[1]] - sum(true_anomalies .* xx[1, :]))                   # true anomalies
+@constraint(m, [i=2:n-1], ta[i] == sum(true_anomalies .* xx[i-1, :]) - 
+                                    sum(true_anomalies .* xx[i, :])) #TODO fix true anomaly
 
 # Fuel consumption
 @variable(m, masses[1:n_sats-1, 1:5] >= m_empty)
@@ -100,9 +126,10 @@ end
 
 optimize!(m)
 
-println("Orbit altitudes (km) : $([(sum(getvalue.(y)[i,:] .* potential_orbits) - rE)/1e3 for i=1:n_orbits])")
-println("Satellite order: $(append!(max_idx, getvalue.(xx) * collect(1:n)))")
-println("Orbital revolutions: $(abs.(period_sat .* getvalue.(ta) ./ getvalue.(dt_orbit)))")
+println("Orbit altitudes (km) : $(round.([(sum(getvalue.(y)[i,:] .* potential_orbits) - rE)/1e3 for i=1:n_sats-1], sigdigits=5))")
+println("Satellite order: $(Int.(round.(getvalue.(sat_order))))")
+println("True anomalies: $(round.(getvalue.(ta), sigdigits=5))")
+println("Orbital revolutions: $(round.(abs.(period_sat .* getvalue.(ta) ./ getvalue.(dt_orbit)), sigdigits=5))")
 
 
 
