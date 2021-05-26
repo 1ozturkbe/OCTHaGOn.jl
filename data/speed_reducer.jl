ecfd8de5507e4fcabbd04017a9c2269fc13e3387
Berk Ozturk <1ozturkbe@gmail.com>
2021-05-26 19:31:47 -0400

    Speed reducer revamped.

--- data/speed_reducer.jl
+++ data/speed_reducer.jl
@@ -1,6 +1,5 @@
 #=
-Speed reducer problem from [Li, 2017]
-Known optimum is 2999.76.
+Ray, T. (2003). Golinski’s speed reducer problem revisited. AIAA Journal, 41(3), 556–558. https://doi.org/10.2514/2.1984
 =#
 
 function speed_reducer(solver = CPLEX_SILENT)
@@ -13,9 +12,9 @@ function speed_reducer(solver = CPLEX_SILENT)
     bound!(gm, Dict(var => [lbs[var], ubs[var]] for var in gm.vars))
 
 #     Objective
-    add_nonlinear_constraint(gm, :(x -> 0.7854*x[1]*x[2]^2*(10. / 3. *x[3]^2 + 14.9334*x[3] - 43.0934) -
-             1.508*x[1]*(x[6]^2 + x[7]^2) + 7.477*(x[6]^3 + x[7]^3) +
-             0.7854*(x[4]*x[6]^2 + x[5]*x[7]^2)), vars = x[1:7], dependent_var = x[8])
+    add_nonlinear_constraint(gm, :(x -> x[8] - (0.7854*x[1]*x[2]^2*(10. / 3. *x[3]^2 + 14.9334*x[3] - 43.0934) -
+             1.5079*x[1]*(x[6]^2 + x[7]^2) + 7.477*(x[6]^3 + x[7]^3) +
+             0.7854*(x[4]*x[6]^2 + x[5]*x[7]^2))), vars = x[1:8])
 
     # Constraints
     add_nonlinear_constraint(gm, :(x -> -27 + x[1] * x[2]^2 * x[3]), vars = x[1:3])
@@ -26,21 +25,23 @@ function speed_reducer(solver = CPLEX_SILENT)
 
     add_nonlinear_constraint(gm, :(x -> -1.93 + x[2] * x[7]^4 * x[3] / x[5]^3), vars = [x[2], x[3], x[5], x[7]])
 
-    add_nonlinear_constraint(gm, :(x -> 1100 - (((745*x[4]/(x[2]*x[3]))^2) +
-                                            16.91*10^6)^0.5/(0.1*x[6]^3)), vars = [x[2], x[3], x[4], x[6]])
+    add_nonlinear_constraint(gm, :(x -> 110.0*x[6]^3 - (((745*x[4]/(x[2]*x[3]))^2) +
+    16.91*10^6)^0.5), vars = [x[2], x[3], x[4], x[6]])
 
-    add_nonlinear_constraint(gm, :(x -> 850 - (((745*x[5]/(x[2]*x[3]))^2) +
-                                            157.5*10^6)^0.5/(0.1*x[7]^3)), vars = [x[2], x[3], x[5], x[7]])
+    add_nonlinear_constraint(gm, :(x -> 85.0*x[7]^3 - (((745*x[5]/(x[2]*x[3]))^2) +
+                                            157.5*10^6)^0.5), vars = [x[2], x[3], x[5], x[7]])
 
     add_nonlinear_constraint(gm, :(x -> 40 - x[2]*x[3]), vars = [x[2], x[3]])
 
-    add_nonlinear_constraint(gm, :(x -> x[1]/x[2] - 5), vars = [x[1], x[2]])
+    add_nonlinear_or_compatible(gm, :(x -> x[1] - 5*x[2]), vars = [x[1], x[2]])
 
-    add_nonlinear_constraint(gm, :(x -> 12 - x[1]/x[2]), vars = [x[1], x[2]])
+    add_nonlinear_or_compatible(gm, :(x -> 12*x[2] - x[1]), vars = [x[1], x[2]])
 
     add_nonlinear_or_compatible(gm, :(x -> x[4] - 1.5*x[6] - 1.9), vars = [x[4], x[6]])
 
-#     It turns out constraint g11 is actually completely infeasible... how could they publish these results?
-#     add_nonlinear_constraint!(gm, :(x -> x[5] - 1.5*x[7] - 1.9), vars = [x[5], x[7]])
+    add_nonlinear_or_compatible(gm, :(x -> x[5] - 1.1*x[7] - 1.9), vars = [x[5], x[7]])
+    
+    set_param(gm, :ignore_accuracy, true)
+    
     return gm
 end
