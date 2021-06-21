35a35aad421a6ff057be7c301a7c3371fc8ffdb1
Berk Ozturk <1ozturkbe@gmail.com>
2021-06-21 10:35:17 -0400

    Feas_gaps are relative, as they should be...

--- src/global_model.jl
+++ src/global_model.jl
@@ -418,10 +418,10 @@ function feas_gap(gm::GlobalModel)
     for bbl in gm.bbls
         if bbl isa BlackBoxClassifier && !isnothing(bbl.constraint)
             eval!(bbl, soln)
-            push!(bbl.feas_gap, bbl.Y[end])
+            push!(bbl.feas_gap, bbl.Y[end] ./ (maximum(bbl.Y) - minimum(bbl.Y)))
             for ll in bbl.lls
                 eval!(bbl, DataFrame(string.(bbl.vars) .=> values(soln[1, string.(ll.vars)])))
-                push!(ll.feas_gap, bbl.Y[end])
+                push!(ll.feas_gap, bbl.Y[end] ./ (maximum(bbl.Y) - minimum(bbl.Y)))
             end
         elseif bbl isa BlackBoxRegressor && !isnothing(bbl.constraint)
             eval!(bbl, soln)
@@ -429,14 +429,14 @@ function feas_gap(gm::GlobalModel)
             actual = bbl.Y[end]
             push!(bbl.optima, optimum)
             push!(bbl.actuals, actual)
-            push!(bbl.feas_gap, optimum-actual)
+            push!(bbl.feas_gap, (optimum-actual) / (maximum(bbl.Y) - minimum(bbl.Y)))
             for ll in bbl.lls
                 eval!(bbl, DataFrame(string.(bbl.vars) .=> values(soln[1, string.(ll.vars)])))
                 optimum = JuMP.getvalue(ll.dependent_var)
                 actual = bbl.Y[end]
                 push!(ll.optima, optimum)
                 push!(ll.actuals, actual)
-                push!(ll.feas_gap, optimum-actual)
+                push!(ll.feas_gap, (optimum-actual) / (maximum(bbl.Y) - minimum(bbl.Y)))
             end
         elseif bbl isa BlackBoxClassifier && isnothing(bbl.constraint)
             push!(bbl.feas_gap, 0) # data constraints are always feasible
