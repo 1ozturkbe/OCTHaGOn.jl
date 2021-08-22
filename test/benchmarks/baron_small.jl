c079c58fbed52f9984d791c0ae6360a7a6118e58
Berk Ozturk <1ozturkbe@gmail.com>
2021-08-22 18:00:49 -0400

    baron_small benchmarks.

---
+++ test/benchmarks/baron_small.jl
@@ -0,0 +1,17 @@
+include("test/load.jl")
+
+# Loading BARON examples (that haven't been loaded yet)
+for i in ["nlp1.jl", "nlp2.jl", "nlp3.jl"]
+    include(OCT.BARON_DIR * i)
+end
+
+BARON_PROBLEMS = [minlp, pool1, nlp1, nlp2, nlp3]
+
+bms = [p(false) for p in BARON_PROBLEMS]
+globalsolve_and_time!.(bms)
+
+gms = [p(true) for p in BARON_PROBLEMS]
+globalsolve_and_time!.(gms)
+
+# save_fit.(gms)
+# load_fit.(gms)
