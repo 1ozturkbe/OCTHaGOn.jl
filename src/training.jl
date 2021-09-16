32164aefb4a724511ea3ef5e5d8a1e20baec8405
Berk Ozturk <1ozturkbe@gmail.com>
2021-09-16 14:04:34 -0400

    Starting to think about tree training using MIO.

---
+++ src/training.jl
@@ -0,0 +1,12 @@
+# Parameters
+random_seed = 1
+max_depth = 5
+cp = 1e-6
+minbucket = 0.01
+fast_num_support_restarts = 5
+localsearch = true
+ls_num_tree_restarts = 10
+ls_num_hyper_restarts = 5
+
+
+
