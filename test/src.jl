b327d91fea7497198ec97138ecfa98b59ca0574c
Berk Ozturk <1ozturkbe@gmail.com>
2021-09-16 14:53:29 -0400

    Reducing problem size for convenience.

--- test/src.jl
+++ test/src.jl
@@ -607,8 +607,7 @@ end
 # Predator prey model with logistic function from http://www.math.lsa.umich.edu/~rauch/256/F2Lab5.pdf
 function test_linking()
     m = Model(CPLEX_SILENT)
-    MOI.set(m, MOI.RawParameter("CPX_PARAM_DETTILIM"), 1)
-    t = 30
+    t = 15
     r = 0.2
     x1 = 0.6
     y1 = 0.5
