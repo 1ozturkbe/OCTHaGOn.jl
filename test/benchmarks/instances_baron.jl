9df86b4cfc28e6f8526f1093ec353a7ecaa5c6b6
Berk Ozturk <1ozturkbe@gmail.com>
2021-12-06 11:06:18 -0500

    More clean-ups.

--- test/benchmarks/instances_baron.jl
+++ test/benchmarks/instances_baron.jl
@@ -8,22 +8,14 @@ while !eof(file)
 
  for name in gams_problem_names
     try
-        gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+        gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
         @info "$(name) worked."
     catch
         @info "$(name) didn't work."
     end
  end
 
- name = "ex6_2_10.gms"
- name = "ex8_4_3.gms"
  name = "flay05m.gms" # GOOD EXAMPLE
- name = "pointpack08.gms" # GOOD EXAMPLE
- name = "ramsey.gms"
- gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+ gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
+ set_optimizer(gm, CPLEX_SILENT)
  globalsolve_and_time!(gm)
-
- add_relaxation_variables!(gm)
- relax_objective!(gm)
- add_tree_constraints!(gm)
- optimize!(gm)
--- test/benchmarks/largebench.jl
+++ test/benchmarks/largebench.jl
@@ -11,19 +11,19 @@
 # globalsolve_and_time!(gm)
 
 # name = "ex7_2_3.gms"
-# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 # set_param(gm, :step_penalty, 1e12)
 # set_optimizer(gm.model, CPLEX_SILENT)
 # globalsolve_and_time!(gm)
 
 # name = "ex8_4_3.gms"
-# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 # set_optimizer(gm.model, CPLEX_SILENT)
 # globalsolve_and_time!(gm)
 
 # # LOGARITHM CAUSING ISSUES
 # name = "ramsey.gms"
-# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 # JuMP.set_upper_bound.(gm.vars, 4*ones(length(gm.vars))) # ramsey
 # JuMP.set_lower_bound.(all_variables(gm.bbls), zeros(length(all_variables(gm.bbls)))) # ramsey
 # set_optimizer(gm.model, CPLEX_SILENT)
@@ -31,11 +31,9 @@
 
 # # Causing errors in hyperplane splits
 # name = "wastepaper4.gms"
-# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 # set_optimizer(gm.model, CPLEX_SILENT)
 # globalsolve_and_time!(gm)
-
-include("../bench.jl")
 
 function optimize_with_more_depth(gm::GlobalModel)
     set_param(gm, :sample_coeff, 400)
@@ -47,7 +45,7 @@ end
 
 # ALL SET TO GO 
 name = "himmel16.gms"
-gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 set_optimizer(gm, CPLEX_SILENT)
 JuMP.set_upper_bound.(gm.vars, ones(length(gm.vars))) # himmel16
 JuMP.set_lower_bound.(gm.vars, -ones(length(gm.vars))) # himmel16
@@ -57,15 +55,17 @@ optimize_with_more_depth(gm)
 
 # ALL SET TO GO 
 name = "kall_circles_c6b.gms"
-gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 set_optimizer(gm.model, CPLEX_SILENT)
+set_param(gm, :step_penalty, 1e8)
+set_param(gm, :equality_penalty, 1e8)
 time1 = time()
 optimize_with_more_depth(gm)
 @info "Time elapsed: $(time()-time1)"
 
 # ALL SET TO GO 
 name = "pointpack08.gms"
-gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 time1 = time()
 set_optimizer(gm, CPLEX_SILENT)
 bound!(gm.model, gm.model[:obj] => [0, 0.5])
@@ -74,7 +74,7 @@ optimize_with_more_depth(gm)
 
 # ALL SET TO GO 
 name = "flay05m.gms"
-gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 set_optimizer(gm, CPLEX_SILENT)
 time1 = time()
 optimize_with_more_depth(gm)
@@ -82,7 +82,7 @@ optimize_with_more_depth(gm)
 
 # ALL SET TO GO
 name = "fo9.gms"
-gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 set_optimizer(gm, CPLEX_SILENT)
 time1 = time()
 optimize_with_more_depth(gm)
@@ -91,7 +91,7 @@ optimize_with_more_depth(gm)
 
 # ALL SET TO GO 
 name = "o9_ar4_1.gms"
-gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 set_optimizer(gm.model, CPLEX_SILENT)
 time1 = time()
 optimize_with_more_depth(gm)
@@ -99,7 +99,7 @@ optimize_with_more_depth(gm)
 
 # # ALL SET, but no comparative solver
 # name = "sfacloc2_3_80.gms"
-# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR * "\\gms\\", name)
+# gm = GAMS_to_GlobalModel(OCT.GAMS_DIR, name)
 # set_param(gm, :max_iterations, 500)
 # set_param(gm, :ignore_accuracy, true)
 # set_optimizer(gm, CPLEX_SILENT)
