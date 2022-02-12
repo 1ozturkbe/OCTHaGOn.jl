34340c2ee04f9267645c48fe9cc28909aed2f74e
1ozturkbe <1ozturkbe@gmail.com>
2022-02-12 14:58:31 +0000

    First working docs build.

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,27 @@
+using Pkg
+
+include("../test/load.jl")
+
+Pkg.activate("docs/.")
+
 using Documenter
 using DocStringExtensions
 
 makedocs(sitename = "OCTHaGOn",
-        format = Documenter.HTML(
-            prettyurls = get(ENV, "CI", nothing) == "true"
-        )
-        )
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    modules = [OCTHaGOn],
+    pages = [
+        "Home" => "index.md",
+        "Installation" => "installation.md",
+        "Basic usage" => "basic.md",
+        "API reference" => [
+            "api/helpers.md"
+        ]
+    ]
+)
+
+deploydocs(
+    repo = "github.com/1ozturkbe/OCTHaGOn.jl.git",
+    target = "build",
+    push_preview = true,
+)
