c08fab6dd37888fd555c476717a75f6630d28b6a
1ozturkbe <1ozturkbe@gmail.com>
2022-07-21 21:00:53 -0400

    Adding BARON compiling to test/bench.jl

--- test/bench.jl
+++ test/bench.jl
@@ -1,4 +1,9 @@
 include("load.jl")
+
 gm = nlp2(true)
 set_optimizer(gm, SOLVER_SILENT)
 globalsolve_and_time!(gm)
+
+using BARON
+gm = GAMS_to_baron_model("../OCTHaGOn_benchmarks/gams/global/", "ex4_1_1.gms")
+optimize!(gm)
